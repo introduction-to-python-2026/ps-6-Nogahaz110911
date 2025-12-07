8c0e650d2e5fb5422b78fe0fc8a56201c3144c94
Nogahaz110911 <nogahaziza@gmail.com>
2025-12-07 17:26:35 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,11 +1,11 @@
 def create_codon_dict(file_path):
-    file = open(file_path, "r")
-    rows = file.readlines()
-    file.close()
+    with open(file_path, "r") file:
+        rows = file.readlines()
     codons = {}
     for row in rows:
-        row.strip().split('\t')
-        codons.update({row[1] : row[3]})
+        columns = row.strip().split('\t')
+        if len(columns) > 3:
+            codons.update({columns[1]: columns[3]})
     return codons
 
 
