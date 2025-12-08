a86e9ba4c3585b369bdc68ca092d0b0c0efa6c93
Nogahaz110911 <nogahaziza@gmail.com>
2025-12-08 20:22:35 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,11 +1,11 @@
 def create_codon_dict(file_path):
-    codon_dict = {}
-    with open(file_path, 'r') as f:
-        for line in f:
-            clean_line = line.strip()
-            if clean_line:
-                parts = clean_line.split()
-                if len(parts) == 2:
-                    codon, amino_acid = parts
-                    codon_dict[codon] = amino_acid
-    return **codon_dict**
+    file = open(file_path)
+    rows = file.readlines()
+    file.close()
+    dic = {}
+    for row in rows:
+        cells = row.strip().split('\t')
+        codon = cells[0]
+        amino_acid = cells[2]
+        dic[codon] = amino_acid
+    return dic
